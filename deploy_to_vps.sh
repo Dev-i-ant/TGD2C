57de87d027a6c2c60e726505c655903d15d72a7a
Vladimir Krasov <vdaper@yandex.ru>
2026-02-07 13:48:40 +0300

    add: сертификаты (не знаю зачем они тут вообще)

--- deploy_to_vps.sh
+++ deploy_to_vps.sh
@@ -12,10 +12,20 @@ rsync -avz -e "ssh -i ~/.ssh/reg_ru" \
 echo "🛠️ Пересборка и запуск на сервере..."
 ssh -i ~/.ssh/reg_ru root@89.111.175.31 << EOF
 cd /root/mini_app
+mkdir -p /etc/nginx/ssl
 docker compose -f docker-compose.vps.yml build
 docker compose -f docker-compose.vps.yml up -d
-# Update nginx config
-cp nginx.conf /etc/nginx/sites-available/default
+EOF
+
+echo "🔒 Синхронизация SSL сертификатов..."
+scp -i ~/.ssh/reg_ru ./ssl/back-loot.online.crt root@89.111.175.31:/etc/nginx/ssl/
+scp -i ~/.ssh/reg_ru ./ssl/back-loot.online.key root@89.111.175.31:/etc/nginx/ssl/
+scp -i ~/.ssh/reg_ru ./ssl/back-loot.ru.crt root@89.111.175.31:/etc/nginx/ssl/
+scp -i ~/.ssh/reg_ru ./ssl/back-loot.ru.key root@89.111.175.31:/etc/nginx/ssl/
+
+echo "📢 Обновление конфигурации Nginx..."
+ssh -i ~/.ssh/reg_ru root@89.111.175.31 << EOF
+cp /root/mini_app/nginx.conf /etc/nginx/sites-available/default
 nginx -t && systemctl reload nginx
 EOF
 

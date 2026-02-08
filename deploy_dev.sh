0beaf77652094d50894a3242400a28a81fd8dc31
Vladimir Krasov <vdaper@yandex.ru>
2026-02-08 21:20:30 +0300

    upd: разделение на прод и дев среду

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -2,15 +2,16 @@
 
 # Configuration
 SERVER_IP="89.111.175.31"
-REMOTE_DIR="/root/mini_app"
+REMOTE_DIR="/root/mini_app_dev"
 SSH_KEY="~/.ssh/reg_ru"
 
 echo "🚀 Синхронизация файлов для DEV..."
-rsync -avz --exclude 'node_modules' --exclude '.next' --exclude '.git' --exclude 'dev.db' --exclude 'dev_stg.db' -e "ssh -i $SSH_KEY" ./ root@$SERVER_IP:$REMOTE_DIR/
+rsync -avz --exclude 'node_modules' --exclude '.next' --exclude '.git' --exclude 'dev.db' --exclude 'dev_stg.db' --exclude '.env*' -e "ssh -i $SSH_KEY" ./ root@$SERVER_IP:$REMOTE_DIR/
+scp -i $SSH_KEY .env.dev root@$SERVER_IP:$REMOTE_DIR/.env
 
 echo "🛠️ Пересборка и запуск DEV на сервере..."
 ssh -i $SSH_KEY root@$SERVER_IP << 'EOF'
-    cd /root/mini_app
+    cd /root/mini_app_dev
     # Ensure prisma directory exists and is writable
     mkdir -p prisma
     
@@ -33,10 +34,6 @@ ssh -i $SSH_KEY root@$SERVER_IP << 'EOF'
     docker exec -u 1001 -e DATABASE_URL=file:/app/prisma/dev.db TGD2OC_web_dev npx prisma db push --accept-data-loss
     docker exec -u 1001 -e DATABASE_URL=file:/app/prisma/dev.db TGD2OC_web_dev npx tsx prisma/seed_test_case.ts
     
-    # Update .env with API Key
-    echo "MARKET_API_KEY=\"m7guIEV2RUy4s6SmxueDBSzs07Z2eHc\"" >> .env.tmp
-    # Use awk to deduplicate keys, keeping the last one
-    awk -F= '!a[$1]++' .env.tmp .env > .env.new && mv .env.new .env && rm .env.tmp
     docker compose -f docker-compose.dev.yml restart web-dev
 
     # Update nginx config as well
--- deploy_to_vps.sh
+++ deploy_to_vps.sh
@@ -6,12 +6,14 @@ rsync -avz -e "ssh -i ~/.ssh/reg_ru" \
     --exclude "node_modules" \
     --exclude ".next" \
     --exclude ".git" \
-    --exclude ".env" \
-    ./ root@89.111.175.31:/root/mini_app/
+    --exclude ".env*" \
+    ./ root@89.111.175.31:/root/mini_app_prod/
+
+scp -i ~/.ssh/reg_ru .env.prod root@89.111.175.31:/root/mini_app_prod/.env
 
 echo "🛠️ Пересборка и запуск на сервере..."
 ssh -i ~/.ssh/reg_ru root@89.111.175.31 << EOF
-cd /root/mini_app
+cd /root/mini_app_prod
 mkdir -p /etc/nginx/ssl
 docker compose -f docker-compose.vps.yml build
 docker compose -f docker-compose.vps.yml up -d
@@ -25,7 +27,7 @@ scp -i ~/.ssh/reg_ru ./ssl/back-loot.ru.key root@89.111.175.31:/etc/nginx/ssl/
 
 echo "📢 Обновление конфигурации Nginx..."
 ssh -i ~/.ssh/reg_ru root@89.111.175.31 << EOF
-cp /root/mini_app/nginx.conf /etc/nginx/sites-available/default
+cp /root/mini_app_prod/nginx.conf /etc/nginx/sites-available/default
 nginx -t && systemctl reload nginx
 EOF
 
